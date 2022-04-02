af658bddbd9c75009dbd35dbc8c244acc737fea6
dawit.abera <dawit.y.abera@nordstrom.com>
2022-04-02 12:44:39 -0700

    load env file

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
 	"gitlab.nordstrom.com/go-admin/database"
 	"gitlab.nordstrom.com/go-admin/routes"
+	"gitlab.nordstrom.com/go-admin/utils"
 )
 
 func main() {
+	// Load env files
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	utils.SecretKey = os.Getenv("SECRET")
+
 	// Create db connection
 	database.Connect()
 
