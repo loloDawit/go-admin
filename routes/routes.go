ae949f9bf9dbd61eeb5a62a8b3a9d4713f1071a2
dawit.abera <dawit.y.abera@nordstrom.com>
2022-04-02 13:29:00 -0700

    update routes

--- routes/routes.go
+++ routes/routes.go
@@ -13,10 +13,18 @@ func SetupRoutes(app *fiber.App) {
 	// private routes
 	app.Use(middlewares.IsAuthenticated)
 
-	app.Get("/api/v1/logout", controllers.Logout)
+	app.Post("/api/v1/logout", controllers.Logout)
 	app.Get("/api/v1/user", controllers.User)
+
 	app.Get("/api/v1/users", controllers.GetAllUsers)
+	app.Post("/api/v1/users", controllers.CreateUser)
 	app.Get("/api/v1/user/:id", controllers.GetUser)
 	app.Put("/api/v1/user/:id", controllers.UpdateUser)
 	app.Delete("/api/v1/user/:id", controllers.DeleteUser)
+	// roles
+	app.Get("/api/v1/roles", controllers.GetAllRoles)
+	app.Post("/api/v1/roles", controllers.CreateRole)
+	app.Get("/api/v1/role/:id", controllers.GetRole)
+	app.Put("/api/v1/role/:id", controllers.UpdateRole)
+	app.Delete("/api/v1/role/:id", controllers.DeleteRole)
 }
