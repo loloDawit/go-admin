c4deaa554e02248847e04da588055821304e0dda
dawit.abera <dawit.y.abera@nordstrom.com>
2022-04-02 13:03:04 -0700

    update user model

--- models/user.go
+++ models/user.go
@@ -1,9 +1,20 @@
 package models
 
+import "golang.org/x/crypto/bcrypt"
+
 type User struct {
 	Id        int    `json:"id"`
 	FirstName string `json:"firstname"`
 	LastName  string `json:"lastname"`
 	Email     string `json:"email" gorm:"unique"`
 	Password  []byte `json:"-"`
+}
+
+func (user *User) SetPassword(password string) {
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte("test"), 14)
+	user.Password = hashedPassword
+}
+
+func (user *User) CompareHashAndPassword(password string) error {
+	return bcrypt.CompareHashAndPassword(user.Password, []byte(password))
 }
