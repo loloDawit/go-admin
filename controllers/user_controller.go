e8ad5b211e69024dc22ec8d04018965e25a650f3
dawit.abera <dawit.y.abera@nordstrom.com>
2022-04-02 13:16:17 -0700

    add user CRUD

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 	"gitlab.nordstrom.com/go-admin/database"
 	"gitlab.nordstrom.com/go-admin/models"
@@ -12,6 +14,48 @@ func GetAllUsers(ctx *fiber.Ctx) error {
 	database.DB.Find(&users)
 
 	return ctx.JSON(users)
+}
+
+func GetUser(ctx *fiber.Ctx) error {
+	id, _ := strconv.Atoi(ctx.Params("id"))
+
+	user := models.User{
+		Id: id,
+	}
+
+	database.DB.Find(&user)
+
+	return ctx.JSON(user)
+}
+
+func UpdateUser(ctx *fiber.Ctx) error {
+	id, _ := strconv.Atoi(ctx.Params("id"))
+
+	user := models.User{
+		Id: id,
+	}
+
+	if err := ctx.BodyParser(&user); err != nil {
+		return err
+	}
+
+	database.DB.Model(&user).Updates(user)
+
+	return ctx.JSON(user)
+}
+
+func DeleteUser(ctx *fiber.Ctx) error {
+	id, _ := strconv.Atoi(ctx.Params("id"))
+
+	user := models.User{
+		Id: id,
+	}
+
+	database.DB.Delete(&user)
+
+	return ctx.JSON(fiber.Map{
+		"msg": "success",
+	})
 }
 
 func CreateUser(ctx *fiber.Ctx) error {
