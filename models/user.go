b3e17c638126816839cfda455f7861a3715be845
dawit.abera <dawit.y.abera@nordstrom.com>
2022-04-01 22:17:40 -0700

    update user model

--- models/user.go
+++ models/user.go
@@ -1,9 +1,9 @@
 package models
 
 type User struct {
-	Id        int
-	FirstName string
-	LastName  string
-	Email     string `gorm:"unique"`
-	Password  []byte
+	Id        int    `json:"id"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Email     string `json:"email" gorm:"unique"`
+	Password  []byte `json:"-"`
 }
