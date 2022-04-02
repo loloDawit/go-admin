b119f8323858b42dafd8bb6a15b7231239f2d4ed
dawit.abera <dawit.y.abera@nordstrom.com>
2022-04-01 18:24:42 -0700

    update db connection

--- database/db.go
+++ database/db.go
@@ -1,16 +1,18 @@
 package database
 
 import (
+	"gitlab.nordstrom.com/go-admin/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func Connect() {
 	dns := "test-user:password@/go_admin"
-	_, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
 
 	if err != nil {
 		panic("Could not connect to the database")
 	}
 
+	db.AutoMigrate(&models.User{})
 }
