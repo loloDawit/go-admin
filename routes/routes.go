969541b1ecad2858dd8d0fbf7d64c87da3db1d58
dawit.abera <dawit.y.abera@nordstrom.com>
2022-04-02 12:44:00 -0700

    add security to routes

--- routes/routes.go
+++ routes/routes.go
@@ -3,11 +3,16 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"gitlab.nordstrom.com/go-admin/controllers"
+	"gitlab.nordstrom.com/go-admin/middlewares"
 )
 
 func SetupRoutes(app *fiber.App) {
 	app.Post("/api/v1/register", controllers.Register)
 	app.Post("/api/v1/login", controllers.Login)
+
+	// private routes
+	app.Use(middlewares.IsAuthenticated)
+	
 	app.Get("/api/v1/logout", controllers.Logout)
 	app.Get("/api/v1/user", controllers.User)
 }
