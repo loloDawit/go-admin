5f2a77d2146ef0cca0f5d112be73745d9cf29bbf
loloDawit <dawitabera64@gmail.com>
2022-04-04 10:22:58 -0700

    Add additional func to export and show charts

--- controllers/order_controller.go
+++ controllers/order_controller.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"encoding/csv"
+	"os"
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
@@ -64,4 +66,67 @@ func CreateOrder(ctx *fiber.Ctx) error {
 
 	database.DB.Create(&order)
 	return ctx.JSON(order)
+}
+
+func Export(ctx *fiber.Ctx) error {
+	filePath := "./csv/orders.csv"
+	if err := CreateFile(filePath); err != nil {
+		return err
+	}
+	return ctx.Download(filePath)
+}
+
+func CreateFile(filePath string) error {
+	file, err := os.Create(filePath)
+
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	var orders []models.Order
+
+	database.DB.Preload("OrderItems").Find(&orders)
+	writer.Write([]string{
+		"ID", "Name", "Email", "Product Title", "Price", "Quantity",
+	})
+
+	for _, order := range orders {
+		data := []string{
+			strconv.Itoa(int(order.Id)), order.FirstName + " " + order.LastName, order.Email, "", "", "",
+		}
+		if err := writer.Write(data); err != nil {
+			return err
+		}
+
+		for _, orderItem := range order.OrderItems {
+			data = []string{
+				"", "", "", orderItem.ProductTitle, strconv.Itoa(int(orderItem.Price)), strconv.Itoa(int(orderItem.Quantity)),
+			}
+			if err := writer.Write(data); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+type Sales struct {
+	Date string `json:"date"`
+	Sum  string `json:"sum"`
+}
+
+func Chart(ctx *fiber.Ctx) error {
+	var sales []Sales
+
+	database.DB.Raw(`
+		SELECT DATE_FORMAT (o.created_at, '%Y-%m-%d') AS date, SUM(oi.price * oi.quantity) as sum
+		FROM orders o JOIN order_items oi on o.id = oi.order_id
+		GROUP BY date;
+      `).Scan(&sales)
+
+	return ctx.JSON(sales)
 }
