a9b3d2a6c95fe70d63c3373836bf605839aed961
loloDawit <dawitabera64@gmail.com>
2022-04-03 18:24:13 -0700

    Update Order model to add additional fields that don't need to be saved in the DB

--- models/order.go
+++ models/order.go
@@ -1,13 +1,17 @@
 package models
 
+import "gorm.io/gorm"
+
 type Order struct {
-	Id        uint   `json:"id"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastLame"`
-	Email     string `json:"email"`
-	UpdatedAt string `json:"updatedAt"`
-	CreatedAt string `json:"createdAt"`
-	OrderItem []OrderItem `json:"orderItems" gorm:"foreignKey:OrderId"`
+	Id         uint        `json:"id"`
+	FirstName  string      `json:"-"`
+	LastName   string      `json:"-"`
+	Name       string      `json:"name" gorm:"-"`
+	Total      float32     `json:"total" gorm:"-"`
+	Email      string      `json:"email"`
+	UpdatedAt  string      `json:"updatedAt"`
+	CreatedAt  string      `json:"createdAt"`
+	OrderItems []OrderItem `json:"orderItems" gorm:"foreignKey:OrderId"`
 }
 
 type OrderItem struct {
@@ -15,5 +19,28 @@ type OrderItem struct {
 	OrderId      uint    `json:"orderId"`
 	ProductTitle string  `json:"productTitle"`
 	Price        float32 `json:"price"`
-	Quantity     uint    `json:"updatedAt"`
+	Quantity     uint    `json:"quantity"`
+}
+
+func (order *Order) Count(db *gorm.DB) int64 {
+	var total int64
+	db.Model(&Order{}).Count(&total)
+
+	return total
+}
+
+func (order *Order) Take(db *gorm.DB, limit int, offset int) interface{} {
+	var orders []Order
+
+	db.Preload("OrderItems").Offset(offset).Limit(limit).Find(&orders)
+
+	for i, _ := range orders {
+		var total float32 = 0
+		for _, orderItem := range orders[i].OrderItems {
+			total += orderItem.Price * float32(orderItem.Quantity)
+		}
+		orders[i].Name = orders[i].FirstName + " " + orders[i].LastName
+		orders[i].Total = total
+	}
+	return orders
 }
