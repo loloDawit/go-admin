db3953bb4eba60776faad18212bbaf95cc66aaa3
loloDawit <dawitabera64@gmail.com>
2022-04-06 21:09:28 -0700

    update navbar

--- clients/src/Components/Nav.tsx
+++ clients/src/Components/Nav.tsx
@@ -1,32 +1,56 @@
-import { FunctionComponent } from 'react';
+import axios from 'axios';
+import { FunctionComponent, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { UserInfo } from '../interfaces/user';
 
 interface NavProps {}
 
 const Nav: FunctionComponent<NavProps> = () => {
+  const [user, setUser] = useState<UserInfo | null>(null);
+
+  const logout = async () => {
+    var config = {
+      method: 'post',
+      url: 'http://localhost:8080/api/v1/logout',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      withCredentials: true
+    };
+    //@ts-ignore
+    await axios(config);
+  };
+  useEffect(() => {
+    (async () => {
+      var config = {
+        method: 'get',
+        url: 'http://localhost:8080/api/v1/user',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        withCredentials: true
+      };
+      //@ts-ignore
+      const { data } = await axios(config);
+      setUser(data);
+    })();
+  }, []);
+
   return (
-    <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-      <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
+    <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+      <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">
         Go React Admin
       </a>
-      <button
-        className="navbar-toggler position-absolute d-md-none collapsed"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#sidebarMenu"
-        aria-controls="sidebarMenu"
-        aria-expanded="false"
-        aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
-      <div className="navbar-nav">
-        <div className="nav-item text-nowrap">
-          <a className="nav-link px-3" href="#">
-            Sign out
-          </a>
-        </div>
-      </div>
-    </header>
+
+      <ul className="my-2 my-md-0 mr-md-3">
+        <Link to="/profile" className="p-2 text-white text-decoration-none">
+          {user?.firstName}
+        </Link>
+        <Link to="/login" className="p-2 text-white text-decoration-none" onClick={logout}>
+          Sign out
+        </Link>
+      </ul>
+    </nav>
   );
 };
 
